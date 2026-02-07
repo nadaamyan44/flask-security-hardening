fe5eeb0e8329b4c237153bf815b8b4788eb0f49e
nadaamyan44 <n.adaamya44@gmail.com>
2026-02-07 02:21:55 +0200

    Update config.py

--- web/config.py
+++ web/config.py
@@ -1,20 +1,10 @@
 import os
 
-
-with open(os.environ['POSTGRES_USER_FILE']) as f:
-    _db_user = f.read()
-
-with open(os.environ['POSTGRES_PASSWORD_FILE']) as f:
-    _db_pass = f.read()
-
-
-class BaseConfig(object):
-    DEBUG = os.environ['DEBUG']
-    DB_NAME = os.environ['POSTGRES_DB']
-    DB_USER = _db_user
-    DB_PASS = _db_pass
-    DB_PORT = os.environ['DATABASE_PORT']
-    SQLALCHEMY_DATABASE_URI = f'postgresql://{DB_USER}:{DB_PASS}@postgres:{DB_PORT}/{DB_NAME}'
-
-
-pass
+class BaseConfig:
+    POSTGRES_USER = os.environ.get('POSTGRES_USER')
+    POSTGRES_PASSWORD = os.environ.get('POSTGRES_PASSWORD')
+    POSTGRES_DB = os.environ.get('POSTGRES_DB')
+    
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = os.environ.get('SECRET_KEY')
